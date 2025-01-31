e7c4cd7cec1ab477523bc801ea5621d91ffa0fdb
lighthoof <andrew.kuligin@gmail.com>
2025-01-31 21:06:35 +0200

    added command handling logic, added comments

--- repl.go
+++ repl.go
@@ -8,6 +8,7 @@ import (
 	"time"
 )
 
+// a struct to contain supported commands
 type cliCommand struct {
 	name        string
 	description string
@@ -15,25 +16,45 @@ type cliCommand struct {
 }
 
 func startRepl() {
+	//creating a registry of supported commands
+	supportedCommands :=
+		map[string]cliCommand{
+			"exit": {
+				name:        "exit",
+				description: "Exit the Pokedex",
+				callback:    commandExit,
+			},
+		}
+
+	//create an input scanner
 	scanner := bufio.NewScanner(os.Stdin)
 
+	//start input loop
 	for {
 		fmt.Print("Pokedex > ")
 		isScanned := scanner.Scan()
-		if isScanned != true && scanner.Err() != nil {
+		//handle possible input errors
+		if !isScanned /*&& scanner.Err() != nil*/ {
 			fmt.Printf("Error reading input : %v", scanner.Err())
 		}
 
-		//textInput := scanner.Text()
-		commandInput := cleanInput(scanner.Text()) //strings.Fields(strings.ToLower(textInput))
-		fmt.Println("Your command was: " + commandInput[0])
+		//clear the input
+		commandInput := cleanInput(scanner.Text())[0]
+		//handle the command
+		for _, command := range supportedCommands {
+			if commandInput == command.name {
+				command.callback()
+			} else {
+				fmt.Println("Unknown command")
+			}
+		}
 
 	}
 }
 
 func commandExit() error {
-	fmt.Print("Closing the Pokedex... Goodbye!")
-	time.Sleep(2 * time.Second)
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	time.Sleep(1 * time.Second)
 	os.Exit(0)
 	return nil
 }
