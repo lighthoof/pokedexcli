811375770908b2f38c0f62222bb2961c700ecb81
lighthoof <andrew.kuligin@gmail.com>
2025-02-12 21:50:02 +0200

    Refactored API functions into separate files

---
+++ internal/pokeAPIHandler/location_details.go
@@ -0,0 +1,40 @@
+package pokeAPIHandler
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+
+	"github.com/lighthoof/pokedexcli/internal/pokeCache"
+)
+
+func GetPokeAPILocationDetails(url string, GlobalCache *pokeCache.Cache) (LocationDetailResponse, error) {
+	//Send GET request to get location data from provided URL
+	res, err := http.Get(url)
+	if err != nil {
+		return LocationDetailResponse{}, err
+	}
+	defer res.Body.Close()
+
+	//Read body into a json data string
+	jsonData, err := io.ReadAll(res.Body)
+	if err != nil {
+		return LocationDetailResponse{}, err
+	}
+
+	//Check if the record already exists in the cache and decode it
+	var decodedRes LocationDetailResponse
+	cache, ok := GlobalCache.Get(url)
+	if ok {
+		err = json.Unmarshal(cache, &decodedRes)
+	} else {
+		GlobalCache.Add(url, jsonData)
+		err = json.Unmarshal(jsonData, &decodedRes)
+	}
+
+	if err != nil {
+		return LocationDetailResponse{}, err
+	}
+
+	return decodedRes, nil
+}
--- internal/pokeAPIHandler/location_list.go
+++ internal/pokeAPIHandler/location_list.go
@@ -38,34 +38,3 @@ func GetPokeAPILocation(url string, GlobalCache *pokeCache.Cache) (LocationListR
 
 	return decodedRes, nil
 }
-
-func GetPokeAPILocationDetails(url string, GlobalCache *pokeCache.Cache) (LocationDetailResponse, error) {
-	//Send GET request to get location data from provided URL
-	res, err := http.Get(url)
-	if err != nil {
-		return LocationDetailResponse{}, err
-	}
-	defer res.Body.Close()
-
-	//Read body into a json data string
-	jsonData, err := io.ReadAll(res.Body)
-	if err != nil {
-		return LocationDetailResponse{}, err
-	}
-
-	//Check if the record already exists in the cache and decode it
-	var decodedRes LocationDetailResponse
-	cache, ok := GlobalCache.Get(url)
-	if ok {
-		err = json.Unmarshal(cache, &decodedRes)
-	} else {
-		GlobalCache.Add(url, jsonData)
-		err = json.Unmarshal(jsonData, &decodedRes)
-	}
-
-	if err != nil {
-		return LocationDetailResponse{}, err
-	}
-
-	return decodedRes, nil
-}
